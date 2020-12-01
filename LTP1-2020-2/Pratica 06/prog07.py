45458a26df1f0786c27222b51e897d0d6c877763
gio0324 <70949836+gio0324@users.noreply.github.com>
2020-11-30 22:58:57 -0300

    Create prog07.py

---
+++ LTP1-2020-2/Pratica 06/prog07.py
@@ -0,0 +1,16 @@
+#Estrutura de repetição
+numero_secreto = 42
+
+#while significa 'enquanto'
+acertou = False
+while acertou == False:
+  chute = int(input("Informe seu chute:"))
+  if chute > numero_secreto:
+    print('Errrrrrou por alto!')
+  elif chute < numero_secreto:
+    print("Errrou po baixo!")
+  else:
+    acertou = True
+    print("Acertou!!!")
+
+print("Obrigado por jogar!")
